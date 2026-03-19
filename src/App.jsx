5c27da66669af22998ffaa54429a3041c790709a
Fatih Genç <fatihgenc_61@outlook.com>
2026-03-19 09:55:01 +0300

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Learn Longo
+      </h1>
+    </div>
+  )
+}
+
+export default App
